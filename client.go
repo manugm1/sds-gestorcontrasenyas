1b314d5d0e2e2d63939ab9dd55cd3ec40cf9b90c
Manuel <garciamenarguez@gmail.com>
2017-03-28 20:16:33 +0200

    Registro, Login y más

--- client.go
+++ client.go
@@ -2,89 +2,309 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
+	"encoding/base64"
+	"crypto/tls"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
 	"os"
 	"strings"
+	"net/http/cookiejar"
 )
 
-type Usuario struct {
-	Id         int
-	User       string
-	Contraseña string
+// respuesta del servidor
+type Resp struct {
+	Ok  bool   // true -> correcto, false -> error
+	Msg string // mensaje adicional
+}
+
+type Usuario struct{
+	Email string
+	Password string
+}
+
+var SesionIniciada = false //para probar el inicio de sesión y que salga el menú principal.
+														//De momento solo se pone a true cuando hacemos login
+														//Al cerrar sesión se pone a false de nuevo
+														//Esto habría que implementarlo con sesiones reales
+var cookie []*http.Cookie
+var usuarioActual Usuario
+
+/**
+* [1] Operación de registro sobre el servidor
+ */
+func registro() {
+	//Introducir usuario y contraseña
+	fmt.Println("Introduce email: ")
+	email := leerStringConsola()
+
+	fmt.Println("Introduce contraseña: ")
+	password := leerStringConsola()
+
+	//Generamos los parámetros a enviar al servidor
+	parametros := url.Values{}
+	parametros.Set("opcion", "1")
+	//Pasamos el parámetro a la estructura Usuario
+	usuario := Usuario{Email: email, Password: password}
+	parametros.Set("usuario", codificarStructToJSONBase64(usuario))
+
+	//Pasar parámetros al servidor
+	cadenaJSON := comunicarServidor(parametros)
+
+	var respuesta Resp
+	//Des-serializamos el json a la estructura creada
+	error := json.Unmarshal(cadenaJSON, &respuesta)
+	checkError(error)
+
+	//Mostramos sí o sí lo que nos devuelve como respuesta el servidor
+	fmt.Println(respuesta.Msg)
 }
 
 /**
-* Muestra el primer menú: Login, Registro y Salir
+* [2] Operación de login sobre el servidor
+* Prácticamente igual al registro pero tiene en cuenta la sesión
  */
-func primerMenu() {
-	for {
-		fmt.Println("-------Elige opción [1-2] o 'q' para salir-------")
-		fmt.Println("[1] Login")
-		fmt.Println("[2] Registro")
-		fmt.Println("[q] Salir")
-		reader := bufio.NewReader(os.Stdin)
-		opcionElegida, error := reader.ReadString('\n')
+func login(){
+	//Introducir usuario y contraseña
+	fmt.Println("Introduce email: ")
+	email := leerStringConsola()
+
+	fmt.Println("Introduce contraseña: ")
+	password := leerStringConsola()
+
+	//Generamos los parámetros a enviar al servidor
+	parametros := url.Values{}
+	parametros.Set("opcion", "2")
+	//Pasamos el parámetro a la estructura Usuario
+	usuario := Usuario{Email: email, Password: password}
+	parametros.Set("usuario", codificarStructToJSONBase64(usuario))
+
+	//Pasar parámetros al servidor
+	cadenaJSON := comunicarServidor(parametros)
+
+	var respuesta Resp
+	//Des-serializamos el json a la estructura creada
+	error := json.Unmarshal(cadenaJSON, &respuesta)
+	checkError(error)
+	//Manejamos la respuesta de login del servidor
+	if respuesta.Ok {
+		usuarioActual = usuario
+	}
+	//Mostramos sí o sí lo que nos devuelve como respuesta el servidor
+	fmt.Println(respuesta.Msg)
+}
+
+func esLogueado() (bool){
+	devolver := false
+	//Lo primero: asegurarse que el usuario actual tiene valor
+	if usuarioActual != (Usuario{}) {
+		//Generamos los parámetros a enviar al servidor
+		parametros := url.Values{}
+		parametros.Set("opcion", "3")
+		//Pasamos el parámetro a la estructura Usuario
+		parametros.Set("usuario", codificarStructToJSONBase64(usuarioActual))
+
+		//Pasar parámetros al servidor
+		cadenaJSON := comunicarServidor(parametros)
+
+		var respuesta Resp
+		//Des-serializamos el json a la estructura creada
+		error := json.Unmarshal(cadenaJSON, &respuesta)
 		checkError(error)
-		opcionElegida = strings.TrimSpace(strings.Replace(opcionElegida, " ", "", -1)) // quitamos los espacios
+		devolver = respuesta.Ok
+	}
+	return devolver
+}
+
+func logout(){
+	if usuarioActual != (Usuario{}) {
+		//Generamos los parámetros a enviar al servidor
+		parametros := url.Values{}
+		parametros.Set("opcion", "4")
+		//Pasamos el parámetro a la estructura Usuario
+		parametros.Set("usuario", codificarStructToJSONBase64(usuarioActual))
+
+		//Pasar parámetros al servidor
+		cadenaJSON := comunicarServidor(parametros)
+		var respuesta Resp
+		//Des-serializamos el json a la estructura creada
+		error := json.Unmarshal(cadenaJSON, &respuesta)
+		checkError(error)
+		fmt.Println("resultado del logout: "+string(cadenaJSON))
+		//dar de baja el usuarioactual y la cookie
+	}
+}
+
+func listarEntradas(){
+
+}
+
+func anyadirEntrada(){
+
+}
+
+func editarEntrada(){
+
+}
+
+func borrarEntrada(){
+
+}
+
+func main() {
+	fmt.Println("-------GESTOR DE CONTRASEÑAS-------")
+	menu()
+}
+
+/**
+* Maneja el menú, mostrando el menú inicial si NO hay sesión iniciada
+* y menú principal si la sesión SÍ está iniciada
+ */
+func menu() {
+	//Mostrar menú principal del usuario
+	for{
+		//Si no está logueado, mostramos login/registro
+		if !esLogueado() {
+			menuInicio()
+		} else{ //Mostrar menú principal con todas las opciones
+			menuPrincipal()
+		}
+	}
+}
+
+func menuInicio(){
+		fmt.Println("-------Elige opción [1-2] o 'q' para salir-------")
+		fmt.Println("[1] Registro")
+		fmt.Println("[2] Login")
+		fmt.Println("[q] Salir")
+		opcionElegida := leerStringConsola()
+
 		switch opcionElegida {
 		case "1":
-			fmt.Println("Se ha elegido login")
-			login()
-			break
-		case "2":
 			fmt.Println("Se ha elegido registro")
 			registro()
+			break
+		case "2":
+			fmt.Println("Se ha elegido login")
+			login()
 			break
 		case "q", "Q":
 			fmt.Println("Se ha elegido SALIR")
 			os.Exit(1) //finalizamos el programa
 		}
-	}
 }
 
-/**
-* Operación de login sobre el servidor
- */
-func login() {
+func menuPrincipal(){
+		fmt.Println("-------¡Bienvenido!-------")
+		fmt.Println("-------Elige opción [1-4] o 'q' para cerrar sesión-------")
+		fmt.Println("[1] Listar entradas")
+		fmt.Println("[2] Añadir entrada")
+		fmt.Println("[3] Editar entrada")
+		fmt.Println("[4] Borrar entrada")
+		fmt.Println("[q] Cerrar sesión")
+		opcionElegida := leerStringConsola()
 
-}
-
-/**
-* Operación de registro sobre el servidor
- */
-func registro() {
-
-}
-
-func main() {
-
-	fmt.Println("-------GESTOR DE CONTRASEÑAS-------")
-
-	primerMenu()
-
-	/*
-		res1D := &Usuario{
-			Id:         1,
-			User:       "yo",
-			Contraseña: "yo"}
-		res1B, _ := json.Marshal(res1D)
-		fmt.Println(string(res1B))
-
-		if len(os.Args) != 2 {
-			fmt.Fprintf(os.Stderr, "Usage: %s host:port ", os.Args[0])
-			os.Exit(1)
+		switch opcionElegida {
+		case "1":
+			fmt.Println("Se ha elegido listar entradas")
+			listarEntradas()
+			break
+		case "2":
+			fmt.Println("Se ha elegido añadir entrada")
+			anyadirEntrada()
+			break
+		case "3":
+			fmt.Println("Se ha elegido editar entrada")
+			editarEntrada()
+			break
+		case "4":
+			fmt.Println("Se ha elegido borrar entrada")
+			borrarEntrada()
+			break
+		case "q", "Q":
+			fmt.Println("Se ha elegido cerrar sesión")
+			logout()
+			menuInicio() //volvemos al primer menú, será en éste donde se pueda
+									 //salir del programa
+			break
 		}
-		service := os.Args[1]
-		tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
-		checkError(err)
-		conn, err := net.DialTCP("tcp", nil, tcpAddr)
-		checkError(err)
-		_, err = conn.Write([]byte(string(res1B)))
-		checkError(err)
-		result, err := ioutil.ReadAll(conn)
-		checkError(err)
-		fmt.Println(string(result))
-		os.Exit(0)*/
+}
+
+/**
+* Lee un string por consola
+*/
+func leerStringConsola()(string){
+	reader := bufio.NewReader(os.Stdin)
+	lectura, error := reader.ReadString('\n')
+	checkError(error)
+	return strings.TrimSpace(strings.Replace(lectura, " ", "", -1)) // quitamos los espacios
+}
+
+/**
+* Método para comunicar con el servidor pasándole una serie de parámetros (url.Values)
+* Devuelve la respuesta del body codificado en bytes, para que sea el propio
+* método invocador el que parsee como desee la respuesta
+*/
+func comunicarServidor(parametros url.Values)([]byte){
+	jar, _ := cookiejar.New(nil)
+	u, _ := url.Parse("https://localhost:10443")
+	jar.SetCookies(u, cookie)
+	fmt.Println("ANTES RESPUESTA------")
+	for k, v := range cookie {
+				fmt.Println("key:", k, "value:", v)
+	}
+	transporte := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	cliente := &http.Client{Transport: transporte, Jar: jar}
+	//Pasamos los parámetros codificados a base 64 para enviarlos de forma
+	//segura (sin caracteres problemáticos añadidos)
+	peticion, error := cliente.PostForm("https://localhost:10443", parametros)
+	checkError(error)
+
+	//Pasamos el cuerpo de la respuesta del servidor a bytes para devolverlo
+	//El método que invoca a este método ya verá si lo pasa a string o a struct
+	respuesta, error := ioutil.ReadAll(peticion.Body)
+	checkError(error)
+	//Paso más, pasamos a JSON simple, decodificamos JSON base 64
+	respuesta = decodificarJSONBase64ToJSON(string(respuesta))
+
+	fmt.Println("DESPUES RESPUESTA------")
+	//Debug, mostramos las cabeceras header que devuelve el servidor
+	for k, v := range peticion.Cookies() {
+				fmt.Println("key:", k, "value:", v)
+	}
+	cookie = peticion.Cookies()
+	return respuesta
+}
+
+/**
+* Codificamos en JSON una estructura cualquiera y
+* devolvemos codificado el JSON en base64
+*/
+func codificarStructToJSONBase64(estructura interface{})(string){
+	//codificamos en JSON
+	respJSON, error := json.Marshal(&estructura)
+	checkError(error)
+	//codificamos en base64 para que no dé problemas al enviar al servidor
+	respuesta := base64.StdEncoding.EncodeToString(respJSON)
+	return respuesta
+}
+
+/**
+* Decodifica un json en base 64 (viene del cliente así)
+* y lo pasa a []byte que es un json simple que hay que des-serializar
+*/
+func decodificarJSONBase64ToJSON(cadenaCodificada string)([]byte){
+	//Decodificamos el base64
+	cadena, error := base64.StdEncoding.DecodeString(cadenaCodificada)
+	checkError(error)
+
+	//Pasamos a []byte de JSON
+	respuesta := []byte(cadena)
+	return respuesta
 }
 
 /**
--- server.go
+++ server.go
@@ -1,49 +1,318 @@
 package main
 
 import (
+	senyal "context" //tenemos dos context, el por defecto y el de sesiones, renombramos éste
+	"encoding/json"
+	"encoding/base64"
 	"fmt"
-	"net"
+	"strconv"
+	"log"
+	"net/http"
 	"os"
+	"os/signal"
 	"time"
-	//"bufio"
+	"io/ioutil"
+	"github.com/gorilla/context"
+	"github.com/gorilla/sessions"
+	"github.com/gorilla/securecookie"
 )
 
+// respuesta del servidor
+type Resp struct {
+	Ok  bool   // true -> correcto, false -> error
+	Msg string // mensaje adicional
+	SessionId string
+}
+
+type Usuario struct{
+	Email string
+	Password string
+	Entradas map[int] Entrada
+}
+
+type Entrada struct {
+    Login string
+    Password string
+    Web string
+    Descripcion string
+}
+
+//Declaramos y/o inicializamos variables globales
+var rutaBBDD = "bbdd.json"
+var bbdd *os.File
+var usuarios = make(map[string]Usuario)
+var entradas = make(map[int]Entrada)
+var sesiones *sessions.CookieStore
+
+// función para escribir una respuesta del servidor al cliente
+func comunicarCliente(w http.ResponseWriter, estructura interface{}) {
+	w.Write([]byte(codificarStructToJSONBase64(estructura))) // escribimos el JSON resultante
+}
+
 func main() {
-	service := ":8080"
-	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
-	checkError(err)
+	// suscripción SIGINT
+	stopChan := make(chan os.Signal)
+	signal.Notify(stopChan, os.Interrupt)
 
-	listener, err := net.ListenTCP("tcp", tcpAddr)
-	checkError(err)
+	mux := http.NewServeMux()
+	mux.Handle("/", http.HandlerFunc(handler))
+	//El clearHandler es por las sesiones, para no provocar:
+	//"you need to wrap your handlers with context.ClearHandler as or else you will leak memory!"
+	srv := &http.Server{Addr: ":10443", Handler: context.ClearHandler(mux)}
 
-	//Escuchamos peticiones de los clientes
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			continue //Ignoramos el manejador si la petición no es válida
+	//Las opciones de las sesiones
+	sesiones = sessions.NewCookieStore(securecookie.GenerateRandomKey(32))
+	sesiones.Options = &sessions.Options{
+		Path:     "/",
+		MaxAge:   10, // 8 hours
+		HttpOnly: true,
+		Secure: true,
+	}
+
+	go func() {
+		if err := srv.ListenAndServeTLS("cert.pem", "key.pem"); err != nil {
+			log.Printf("listen: %s\n", err)
 		}
-		//Llamamos al manejador de cada cliente
-		go handleClient(conn)
+	}()
+
+	//Abrimos la conexión con la base de datos (fichero)
+	//Para poder obtener la información
+	bbdd, _ = os.Open(rutaBBDD)
+	defer bbdd.Close()
+	cargarDatos()
+
+	<-stopChan // espera señal SIGINT
+
+	//Cuando hemos pulsado ctrl + c (cerrar servidor)
+	//entonces volcamos los datos en la base de datos (fichero bbdd)
+
+	//Parseamos a JSON
+	cadenaJSON, err := json.Marshal(usuarios)
+	checkError(err)
+
+	//Escribimos en el fichero
+	errorEscritura := ioutil.WriteFile(rutaBBDD, cadenaJSON, 0666)
+	checkError(errorEscritura)
+	log.Println("Volcando datos recopilados en la BBDD...")
+
+	//Mostrar apagado de servidor y apagar el servidor realmente
+	log.Println("Apagando servidor ...")
+
+	//Apagar servidor de forma segura
+	ctx, fnc := senyal.WithTimeout(senyal.Background(), 5*time.Second)
+	fnc()
+	srv.Shutdown(ctx)
+	log.Println("Servidor detenido correctamente")
+}
+
+/**
+* Maneja las opciones que indica el cliente
+*/
+func handler(w http.ResponseWriter, request *http.Request) {
+	request.ParseForm() // es necesario parsear el formulario
+	w.Header().Set("Content-Type", "text/plain") // cabecera estándar
+	//Obtenemos la opción/operación solicitada por el cliente
+	opcion := request.Form.Get("opcion")
+
+	if sesion := obtenerSesionPorId(w, request); sesion != nil {
+		//Devolvemos tan solo la cookie del usuario X
+		sesion.Save(request, w)
+	} else if opcion != "1" && opcion != "2" && opcion != "3" {
+		opcion = "0"
+	}
+
+	switch opcion { // comprobamos comando desde el cliente
+		case "0": //caso básico para no permitir
+			resp := Resp{Ok: false, Msg: "Operación no permitida. No ha iniciado sesión."} // formateamos respuesta
+			comunicarCliente(w, resp)
+		case "1": // registro
+			registro(w, request)
+			break
+		case "2": // login
+			login(w, request)
+		case "3": //Es logueado
+			esLogueado(w, request)
+		case "4": //Logout
+			logout(w, request)
+		case "5": //Listar todas las entradas
+
+		case "6": //Listar una entrada por id
+
+		case "7": //Crear entrada
+
+		case "8": //Editar entrada
+
+		case "9": //Borrar entrada
+
+		default:
+			resp := Resp{Ok: false, Msg: "Comando inválido"}    // formateamos respuesta
+			comunicarCliente(w, resp)
 	}
 }
 
-func handleClient(conn net.Conn) {
-	request := make([]byte, 128) // set maximum request length to 128B to prevent flood based attacks
-	defer conn.Close()           //cerramos la conexión sea cual sea el resultado
-	for {
-		read_len, err := conn.Read(request)
-		checkError(err)
-		//Si la lectura ha acabado
-		if read_len == 0 {
-			break // connection already closed by client
-		} else if read_len > 0 {
-			s := string(request[:read_len])
-			fmt.Print("Message Received: ", s)
-		} else {
-			daytime := time.Now().String()
-			conn.Write([]byte(daytime))
-		}
+/**
+* Función que comprueba si existe el usuario en la BBDD
+* Recibe como parámetro el email (clave primaria) del usuario
+*/
+func existeUsuario(email string) (ok bool) {
+	if _, ok = usuarios[email]; ok {
 	}
+	return
+}
+
+/**
+* Registramos al usuario, para ello hay que asegurarse que no exista
+* ya (tener mismo login)
+*/
+func registro(w http.ResponseWriter, request *http.Request){
+	//Viene del cliente codificado en JSON en base64, lo pasamos a JSON simple
+	cadenaJSON := decodificarJSONBase64ToJSON(request.Form.Get("usuario"))
+	var usuario Usuario
+	//Des-serializamos el json a la estructura creada
+	error := json.Unmarshal(cadenaJSON, &usuario)
+	checkError(error)
+	r := Resp{}
+	//Comprobamos que el usuario existe en la base de datos
+	if existeUsuario(usuario.Email){
+		r = Resp{Ok: false, Msg: "El usuario ya existe, vuelve a intentarlo con otros datos."}    // formateamos respuesta
+	}else{
+		//Si no existe, procedemos a crearlo
+		//Inicializamos las entradas como vacías (lógicamente no tiene ninguna)
+		usuario.Entradas = make(map[int]Entrada)
+		//Lo agregamos al mapa global de usuarios
+		usuarios[usuario.Email] = usuario
+		r = Resp{Ok: true, Msg: "Registrado con éxito. Inicia sesión para empezar."}    // formateamos respuesta
+	}
+	comunicarCliente(w, r)
+}
+
+func login(w http.ResponseWriter, request *http.Request){
+	//Viene del cliente codificado en JSON en base64, lo pasamos a JSON simple
+	cadenaJSON := decodificarJSONBase64ToJSON(request.Form.Get("usuario"))
+	var usuario Usuario
+	//Des-serializamos el json a la estructura creada
+	error := json.Unmarshal(cadenaJSON, &usuario)
+	checkError(error)
+	r := Resp{}
+	//Comprobamos que el usuario existe en la base de datos
+	if existeUsuario(usuario.Email){
+		//Ahora comprobamos si Email y Contraseña enviada
+		//desde cliente coincide con lo que tenemos de dicho usuario en la bbdd
+		if usuarios[usuario.Email].Email == usuario.Email &&
+			usuarios[usuario.Email].Password == usuario.Password{
+			//Se crea la sesión
+	    sesion, error2 := sesiones.Get(request, "sesionid")
+	    checkError(error2)
+	    sesion.Values["email"] = usuario.Email
+	    // Guardamos la sesion, antes de que se responda al cliente llamando a "comunicarCliente"
+			sesion.Save(request, w)
+			r = Resp{Ok: true, Msg: "El usuario se ha logueado correctamente."}    // formateamos respuesta
+	  }else{
+			r = Resp{Ok: false, Msg: "No coinciden los parámetros del usuario. Vuelve a intentarlo."}    // formateamos respuesta
+		}
+	}else{
+		r = Resp{Ok: false, Msg: "El usuario no existe, regístrate y vuelve a intentarlo."}    // formateamos respuesta
+	}
+	comunicarCliente(w, r)
+}
+
+/**
+* Comprueba si el usuario está con la sesión iniciada y devuelve una respuesta al cliente
+*/
+func esLogueado(w http.ResponseWriter, request *http.Request) {
+	//Viene del cliente codificado en JSON en base64, lo pasamos a JSON simple
+	cadenaJSON := decodificarJSONBase64ToJSON(request.Form.Get("usuario"))
+	var usuario Usuario
+	//Des-serializamos el json a la estructura creada
+	error := json.Unmarshal(cadenaJSON, &usuario)
+	checkError(error)
+	r := Resp{}
+	sesion, err := sesiones.Get(request, "sesionid")
+	//La sesión (cookie) tiene como valor el email del usuario
+	fmt.Println("EL MAXIMO EDAD ES: "+strconv.Itoa(sesion.Options.MaxAge))
+	if err == nil && !sesion.IsNew && sesion.Options.MaxAge>0 && (sesion.Values["email"] == usuario.Email) {
+		fmt.Println("Logueadoooo")
+		r = Resp{Ok: true, Msg: "El usuario está logueado correctamente."}    // formateamos respuesta
+	} else{
+		fmt.Println("No logueadooo")
+		r = Resp{Ok: false, Msg: "El usuario no está logueado."}    // formateamos respuesta
+	}
+	comunicarCliente(w, r)
+}
+
+func logout(w http.ResponseWriter, request *http.Request){
+	r := Resp{}
+	if sesion := obtenerSesionPorId(w, request); sesion != nil {
+		//Establecemos el valor a vacío y el tiempo de expiración a nulo para borrarlo
+		sesion.Values["email"] = ""
+		sesion.Options.MaxAge = -10
+		sesion.Save(request, w)
+		w.Header().Set("Expires", "Tue, 03 Jul 2001 06:00 GMT")
+		w.Header().Set("Last-Modified", "{now} GMT")
+		w.Header().Set("Cache-Control", "max-age=0, no-cache, must-revalidate, proxy-revalidate")
+
+		r = Resp{Ok: true, Msg: "Sesión cerrada con éxito."}    // formateamos respuesta
+	} else{
+		r = Resp{Ok: false, Msg: "No se ha podido cerrar sesión. Ha ocurrido algún error inesperado."}    // formateamos respuesta
+	}
+	comunicarCliente(w, r)
+}
+/**
+* Comprueba si el usuario está logueado y devuelve la sesión.
+* A diferencia de "esLogueado", este método es interno a nivel del server y no se comunicar
+* con el cliente
+*/
+func obtenerSesionPorId(w http.ResponseWriter, request *http.Request)(*sessions.Session){
+	var respuesta *sessions.Session
+	//Viene del cliente codificado en JSON en base64, lo pasamos a JSON simple
+	cadenaJSON := decodificarJSONBase64ToJSON(request.Form.Get("usuario"))
+	var usuario Usuario
+	//Des-serializamos el json a la estructura creada
+	error := json.Unmarshal(cadenaJSON, &usuario)
+	checkError(error)
+	sesion, err := sesiones.Get(request, "sesionid")
+	//La sesión (cookie) tiene como valor el email del usuario
+	if err == nil && !sesion.IsNew && sesion.Options.MaxAge>0 && (sesion.Values["email"] == usuario.Email) {
+		respuesta = sesion
+	}
+	return respuesta
+}
+
+/**
+* Codificamos en JSON una estructura cualquiera y
+* devolvemos codificado el JSON en base64
+*/
+func codificarStructToJSONBase64(estructura interface{})(string){
+	//codificamos en JSON
+	respJSON, error := json.Marshal(&estructura)
+	checkError(error)
+	//codificamos en base64 para que no dé problemas al enviar al servidor
+	respuesta := base64.StdEncoding.EncodeToString(respJSON)
+	return respuesta
+}
+
+/**
+* Decodifica un json en base 64 (viene del cliente así)
+* y lo pasa a []byte que es un json simple que hay que des-serializar
+*/
+func decodificarJSONBase64ToJSON(cadenaCodificada string)([]byte){
+	//Decodificamos el base64
+	cadena, error := base64.StdEncoding.DecodeString(cadenaCodificada)
+	checkError(error)
+
+	//Pasamos a []byte de JSON
+	respuesta := []byte(cadena)
+	return respuesta
+}
+
+/**
+* Función que se encarga de obtener todos los datos del fichero
+* para trabajar en memoria
+*/
+func cargarDatos(){
+	datosBBDD, error := ioutil.ReadAll(bbdd)
+	checkError(error)
+	json.Unmarshal(datosBBDD, &usuarios)
 }
 
 /**
